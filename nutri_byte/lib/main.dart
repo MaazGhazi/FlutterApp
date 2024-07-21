2badda30647aeb9b784283c252c92a89ace08a85
MaazGhazi <maaz.ghazi1738@gmail.com>
2024-07-21 13:52:19 -0400

    Adding

--- nutri_byte/lib/main.dart
+++ nutri_byte/lib/main.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
-
+var first = Alignment.topCenter;
+var second = Alignment.bottomCenter;
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(109, 176, 66, 66),
-        body: Center(
-          
-          
-          child: Text('Hello World!'),
-        ),
+        body: Lineargradient(),
       ),
     ),
   );
+}
+
+
+class Lineargradient extends StatelessWidget {
+  const Lineargradient({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: BoxDecoration(
+            gradient:  LinearGradient(
+              colors: const [Colors.red, Colors.blue],
+              begin: first,
+              end: second, 
+              ),
+            
+
+          ),
+          child:  CustomeWidget(),
+        );
+  }
+}
+
+class CustomeWidget extends StatelessWidget {
+  const CustomeWidget(this.text, {super.key});
+  final String text;
+  @override
+  Widget build(context) {
+    return   Center(
+          child: Text(text, 
+          style: const TextStyle(color: Colors.green,fontSize: 30.0)
+          ),          
+        );
+  }
 }
